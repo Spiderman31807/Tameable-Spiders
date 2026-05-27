bb4db2e6d26317723e738aeb41cb32e0cf653ef4
Spiderman31807 <Spiderman31807@gmail.com>
2026-05-27 15:27:23 +1000

    Add files via upload

---
+++ models/mojmap-1.20.x/BabyHead.java
@@ -0,0 +1,32 @@
+import tameable.spiders.entity.AgeableSpider;
+
+public class BabyHead<AgeableSpider extends Entity> extends EntityModel<AgeableSpider> {
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
+			new ResourceLocation("modid", "BabyHead"), "main");
+	private final ModelPart head;
+
+	public BabyHead(ModelPart root) {
+		this.head = root.getChild("head");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(32, 4).addBox(-4.0F, -4.0F, -8.0F, 8.0F, 8.0F, 8.0F), PartPose.offset(0.0F, 15.0F, -3.0F));
+		return LayerDefinition.create(meshdefinition, 64, 32);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	public void setupAnim(AgeableSpider entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		this.head.yRot = netHeadYaw / (180F / (float) Math.PI);
+		this.head.xRot = headPitch / (180F / (float) Math.PI);
+		this.head.xScale = 1.25f;
+		this.head.yScale = 1.25f;
+		this.head.zScale = 1.25f;
+	}
+}
